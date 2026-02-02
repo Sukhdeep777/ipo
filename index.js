81cd7dfab446ab5e02b16e661973f1269f6a8471
Sukhdeep Singh Kaur <sukhdeepsinghkaur11@gmail.com>
2026-02-02 14:45:45 +0000

    Pones mas interactividad

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-let main=document.querySelector("#fondo");
+// Para seleccionar al reloj y hacer click encima de este
 let torre=document.querySelector(".torre"); 
 console.log(torre);
-console.log(main);
 torre.addEventListener("click",change)
 function change(){
    document.querySelector("#hora").innerHTML = "<h1>19:00</h1>";
 }
+
+// Para darle click encima de la fogata
+let fecha=document.querySelector("#fecha-fogata")
+console.log(fecha)
+fecha.addEventListener("click",change)
