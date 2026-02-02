d475b2cf9d4030f8d472b429dba7dcb61329b749
Sukhdeep Singh Kaur <sukhdeepsinghkaur11@gmail.com>
2026-02-02 15:34:00 +0000

    Eliminar la actualización de la fecha en la fogata y ajustar la fuente en el estilo del cuerpo

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ let fecha=document.querySelector(".fogata1")
 console.log(fecha)
 fecha.addEventListener("click",change1)
 function change1(){
-   document.querySelector("#fecha").innerHTML = "31 de Febrero"
    console.log(fecha)
    let miAudio= new Audio("sounds/melodia.mp3")
    miAudio.play()
