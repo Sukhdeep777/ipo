db047c573eee0e3bf96d5fb9a3579bf9c17be859
Sukhdeep Singh Kaur <sukhdeepsinghkaur11@gmail.com>
2026-02-02 15:09:14 +0000

    Actualizar la estructura y estilo de la fecha en la fogata, y corregir la selección del elemento en JavaScript

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ torre.addEventListener("click",change)
 function change(){
    document.querySelector("#hora").innerHTML = "<h1>19:00</h1>";
 }
-
+   
 // Para darle click encima de la fogata
-let fecha=document.querySelector("#fecha-fogata")
+let fecha=document.querySelector(".fogata1")
 console.log(fecha)
-fecha.addEventListener("click",change)
+fecha.addEventListener("click",change1)
+function change1(){
+   document.querySelector("#fecha").innerHTML = "31 de Febrero"
+   console.log(fecha)
+}
