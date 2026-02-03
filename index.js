b839b1f11cdbc7f9b834a1c8e0bf75479e9785fa
Sukhdeep777 <singhkaursukhdeep@fpllefia.com>
2026-02-03 09:04:20 +0000

    Acabado

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let torre=document.querySelector(".torre");
 console.log(torre);
 torre.addEventListener("click",change)
 function change(){
-   document.querySelector("#hora").innerHTML = "<h1>19:00</h1>";
+   document.querySelector("#hora").innerHTML = "<h2>19:00</h2>";
 }
    
 // Para darle click encima de la fogata
